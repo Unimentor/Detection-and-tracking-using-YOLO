debb671b2dd16cb6c570a3ec40c59d72446539a8
Abdullah Zafar <108123757+Unimentor@users.noreply.github.com>
2024-11-13 15:58:03 +0500

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -1,9 +1,9 @@
 import cv2 
 import matplotlib as plt
 from ultralytics import YOLO
-model=YOLO("yolov8n.pt")
+model=YOLO("YOLO MODEL") #insert yolo model
 
-def predict(chosen_model, img, classes=[], conf=0.5):
+def predict(chosen_model, img, classes=[], conf=0.5): #confidence level set to 0.5
     if classes:
         results= chosen_model.predict(img, classes=classes, conf=conf)
         return results
@@ -16,9 +16,10 @@ def predict_and_detect(chosen_model, img, classes=[], conf=0.5):
             cv2.rectangle(img, (int(box.xyxy[0][0]), int(box.xyxy[0][1])),(int(box.xyxy[0][2]), int(box.xyxy[0][3])), (255, 0, 0), 2)
             cv2.putText(img, f"{results.names[int(box.cls[0])]}",(int(box.xyxy[0][0]), int(box.xyxy[0][1]) - 10),cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 0), 1)
     return img, results
-path="D:\\courses\\AI_workshop\\04_Computer_Vision\\Images"
-image= cv2.imread(path+"\\car6.png")
-result_img= predict_and_detect(model, image, classes=[2], conf=0.4  )
+path="Enter path" #add one more backward slash with each one in path, e.g. c:\\users\\desktop\\...
+image= cv2.imread(path+"\\file name")
+result_img= predict_and_detect(model, image, classes=[], conf=0.4  ) #enter arguments in classes for detection of objects
+#detailed classes here https://github.com/ultralytics/ultralytics/blob/main/ultralytics/cfg/datasets/coco.yaml
 cv2.imshow("image",image)
-cv2.imwrite("C:\\Users\\lawli\\OneDrive\\Desktop\\BSCE 7",result_img)
+cv2.imwrite("enter path",result_img)
 cv2.waitKey(0)
