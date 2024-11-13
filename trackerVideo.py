f9647171e85c34df7d905fd1ce8b03830df1b39f
Abdullah Zafar <108123757+Unimentor@users.noreply.github.com>
2024-11-13 16:01:31 +0500

    Update trackerVideo.py

--- trackerVideo.py
+++ trackerVideo.py
@@ -1,7 +1,7 @@
 import cv2 
 import matplotlib as plt
 from ultralytics import YOLO
-model=YOLO("yolov8n.pt")
+model=YOLO("YOLO model")
 
 def predict(chosen_model, img, classes=[], conf=0.5):
     if classes:
@@ -16,12 +16,12 @@ def predict_and_detect(chosen_model, img, classes=[], conf=0.5):
             cv2.rectangle(img, (int(box.xyxy[0][0]), int(box.xyxy[0][1])),(int(box.xyxy[0][2]), int(box.xyxy[0][3])), (255, 0, 0), 2)
             cv2.putText(img, f"{results.names[int(box.cls[0])]}",(int(box.xyxy[0][0]), int(box.xyxy[0][1]) - 10),cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 0), 1)
     return img, results
-video_path="D:\\courses\\AI_workshop\\04_Computer_Vision\\Lab 02\\traffic.mkv"
+video_path="Enter video file path with video file"
 cap=cv2.VideoCapture(video_path)
 while True:
     success, img =cap.read()
     if not success:
         break
-    result_img=predict_and_detect(model, img, classes=[2], conf=0.3)
+    result_img=predict_and_detect(model, img, classes=[], conf=0.5) #pass arguments in class based on objects, details of objects here https://github.com/ultralytics/ultralytics/blob/main/ultralytics/cfg/datasets/coco.yaml
     cv2.imshow("image", img)
     cv2.waitKey(1)
