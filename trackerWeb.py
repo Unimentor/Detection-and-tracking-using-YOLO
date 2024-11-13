89ba29d4e4a36d9248e439522a9fc763dc18453b
Abdullah Zafar <108123757+Unimentor@users.noreply.github.com>
2024-11-13 16:03:32 +0500

    Update trackerWeb.py

--- trackerWeb.py
+++ trackerWeb.py
@@ -1,7 +1,7 @@
 import cv2 
 import matplotlib as plt
 from ultralytics import YOLO
-model=YOLO("yolov8n.pt")
+model=YOLO("YOLO model") #enter yolo model
 
 def predict(chosen_model, img, classes=[], conf=0.5):
     if classes:
@@ -16,15 +16,15 @@ def predict_and_detect(chosen_model, img, classes=[], conf=0.5):
             cv2.rectangle(img, (int(box.xyxy[0][0]), int(box.xyxy[0][1])),(int(box.xyxy[0][2]), int(box.xyxy[0][3])), (255, 0, 0), 2)
             cv2.putText(img, f"{results.names[int(box.cls[0])]}",(int(box.xyxy[0][0]), int(box.xyxy[0][1]) - 10),cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 0), 1)
     return img, results
-cap=cv2.VideoCapture(0)
+cap=cv2.VideoCapture(0) 
 while True:
     success, img =cap.read()
     if not success:
         break
-    result_img=predict_and_detect(model, img, classes=[0], conf=0.3)
+    result_img=predict_and_detect(model, img, classes=[], conf=0.3) #pass argument for classes of the objects, details of classes here https://github.com/ultralytics/ultralytics/blob/main/ultralytics/cfg/datasets/coco.yaml
     cv2.imshow("image", img)
     k = cv2.waitKey(100)
-    if k == ord('q'):
+    if k == ord('q'): #press q to stop
         break
 cap.release()
 cv2.destroyAllWindows()
